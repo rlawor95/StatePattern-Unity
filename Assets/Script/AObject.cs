e3a5523f8d889a116c1a8beb2b0ba0536a42c403
kimjaekyoung <jkkim@anymedi.com>
2021-05-25 15:20:55 +0900

    StatePattern

---
+++ Assets/Script/AObject.cs
@@ -0,0 +1,117 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AObject : MonoBehaviour
+{
+    private State<AObject> myState; // 현재 상태를 참조할 변수.
+
+    private void Start()
+    {
+        myState = new AState();
+    }
+
+    private void Update()
+    {
+        State<AObject> nowState = myState.InputHandle(this);
+        myState.action(this);
+
+        if (!nowState.Equals(myState)) //상태가 바뀐 경우
+        {
+            myState.Exit(this);
+            myState = nowState;
+        }
+    }
+
+
+    public class AState : State<AObject>
+    {
+        float time = 0;
+
+        public override State<AObject> InputHandle(AObject t)
+        {
+            if (time > 2.0f)
+                return new BState();
+            return this;
+        }
+        public override void Enter(AObject t)
+        {
+            time = 0;
+            base.Enter(t);
+        }
+
+        public override void Update(AObject t)
+        {
+            Debug.Log("A State Update ");
+            time += Time.deltaTime;
+            base.Update(t);
+        }
+
+        public override void Exit(AObject t)
+        {
+            Debug.Log("A Exit");
+            base.Exit(t);
+        }
+    }
+
+    public class BState : State<AObject>
+    {
+        float time = 0;
+        public override State<AObject> InputHandle(AObject t)
+        {
+            if (time > 2.0f)
+                return new CState();
+            return this;
+        }
+
+        public override void Enter(AObject t)
+        {
+            Debug.Log("B State Enter");
+            base.Enter(t);
+        }
+
+        public override void Update(AObject t)
+        {
+            Debug.Log("B State Update");
+            time += Time.deltaTime;
+            base.Update(t);
+        }
+
+        public override void Exit(AObject t)
+        {
+            Debug.Log("B Exit");
+            base.Exit(t);
+        }
+    }
+
+    public class CState : State<AObject>
+    {
+        float time = 0;
+        public override State<AObject> InputHandle(AObject t)
+        {
+            if (time > 2.0f)
+                return new AState();
+            return this;
+        }
+
+        public override void Enter(AObject t)
+        {
+            time = 0;
+            Debug.Log("C State Enter");
+            base.Enter(t);
+        }
+
+        public override void Update(AObject t)
+        {
+            Debug.Log("C State Update");
+            time += Time.deltaTime;
+            base.Update(t);
+        }
+
+        public override void Exit(AObject t)
+        {
+            Debug.Log("C Exit");
+            base.Exit(t);
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # StatePattern_Unity
+
+예제 설명
  
+AObject 가 상태를 가지는 객체임.
+
+객체는 3가지 상태를 가진다.
+AState, BState, CState 
+2초 간격으로 A-> B-> C -> A 로 무한반복함. 
